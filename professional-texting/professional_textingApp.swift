7e8f1ca17b2f084536eeeb12097fc325afe08faf
Michelle Nathania <mnathaniaa14@gmail.com>
2026-09-03 09:50:05 +0700

    Initial Commit

---
+++ professional-texting/professional_textingApp.swift
@@ -0,0 +1,17 @@
+//
+//  professional_textingApp.swift
+//  professional-texting
+//
+//  Created by Michelle Nathania on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct professional_textingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
